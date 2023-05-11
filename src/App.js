a4df057ecc9354bf292fd2990f9dba55d0877f91
Blaize95 <118699442+Blaize95@users.noreply.github.com>
2023-05-11 14:05:05 +1000

    fixed landing page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
+            <Route path="/" element={<About />} />
             <Route path="About" element={<About />} />
             <Route path="Portfolio" element={<Portfolio />} />
             <Route path="Contact" element={<Contact />} />
